35d7c4efc3c15fb518d10f7470aef7b7fdedae23
root <root@precise32.(none)>
2016-03-29 16:38:48 +0000

    skeleton

---
+++ src/PageGrabber.php
@@ -0,0 +1,6 @@
+<?php namespace AlexGoldovsky\PageGrabber;
+
+class PageGrabber {
+
+}#
+?>
---
+++ test/PageGrabber.test.php
@@ -0,0 +1,9 @@
+<?php namespace AlexGoldovsky\PageGrabber;
+
+class PageGrabberTest extends \PHPUnit_Framework_TestCase {
+
+public function testTrueIsTrue() {
+	$this->assertTrue(true);
+}
+
+}#
