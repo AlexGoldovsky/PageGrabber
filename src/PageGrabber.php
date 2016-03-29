c6e6571cb15423f0d4cb981f64675ff221bb6ae2
root <root@precise32.(none)>
2016-03-29 17:38:28 +0000

    failure check

--- src/PageGrabber.php
+++ src/PageGrabber.php
@@ -1,6 +1,17 @@
 <?php namespace AlexGoldovsky\PageGrabber;
 
 class PageGrabber {
+	
+	private $title;
+	function __construct ($url) {
+		$doc = new DOMDocument();
+		@$doc->loadHTMLFile($url);
+		$this->title = $doc;
+	}
+
+	public function getTitle() {
+		return $this->title;
+	}
 
 }#
 ?>
